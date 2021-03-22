f72ade5b086951fb59ca8dffd940d7ec27ae9d96
prashil13-hub <prashilganvir13@gmail.com>
2021-03-22 19:24:11 +0530

    change

--- Homepage.js
+++ Homepage.js
@@ -1,1 +1,6 @@
-var x=getElement
+let header = document.querySelector('.header');
+let hamburgerMenu = document.querySelector('.hamburger-menu');
+
+hamburgerMenu.addEventListener('click' , function() {
+    header.classList.toggle('menu-open');
+})
